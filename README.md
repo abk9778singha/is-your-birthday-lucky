cdf4d39fd676b94e9b678726faa7d15279ba33ed
Abhishek Singha <95275559+abk9778singha@users.noreply.github.com>
2022-10-05 15:21:59 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# is-your-birthday-lucky
+
+A website which can help you to check whether your birthday is lucky or not. Just for fun not seriously.
+
+## Demo -
+
+You can view this app live here - [Demo](https://)
+
+## Tech Stack -
+
+HTML, JavaScript, CSS for styling
+
+
+## Documentation -
+
+This app is built using HTML, JavaScript and CSS for styling. Helps user to check whether their birthday is lucky or not.
+
+ Features -
+- Take user's complete birthday as input.
+- Take user's lucky number as input.
+- Show the output if the birthdate is lucky or not to the user depending on the calculated results.
+- Show the result to the user with some graphics icon.
+
+
+
+## Screenshots -
+
+<img src="img/birthdaylucky.png" width="500" height="500"/>
+
+
+## Author -
+
+- Abhishek Singha - [abk9778singha](https://github.com/abk9778singha)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,49 @@
+const dateOfBirth = document.querySelector('#date-of-birth');
+const luckyNumber = document.querySelector('#lucky-number');
+const checkNumberButton = document.querySelector('#check-number');
+
+const outputBox = document.querySelector('#output-box');
+
+function compareValues(sum,luckyNumber){
+    // console.log(sum,luckyNumber);
+    if(sum%luckyNumber === 0 ){
+        outputBox.innerText = 'Your birthday is lucky 🥳';
+    }else{
+        outputBox.innerText = 'Your birthday is not lucky😕';
+    }
+}
+
+function checkBirthDateIsLucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if(sum && dob){
+    compareValues(sum,luckyNumber.value)
+    }else{
+        outputBox.innerText = 'Please enter both the fields 👆'
+    }
+}
+
+function calculateSum(dob){
+    dob = dob.replaceAll("-","");
+    let sum = 0;
+    for(let i =0 ;i< dob.length ;i++){
+        sum = sum + Number(dob.charAt(i));
+    }
+    return sum;
+}
+
+checkNumberButton.addEventListener('click',checkBirthDateIsLucky)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
